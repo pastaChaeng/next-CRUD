322039aa7fed712a6eec63255e0f6462f6e29c7b
Yumeko <arlenejoy.nacion777@gmail.com>
2023-09-12 07:58:39 +0800

    api

--- crud/app/api/list/route.ts
+++ crud/app/api/list/route.ts
@@ -1,10 +1,18 @@
-import connectMongoDB from "@/libs/mongodb"
-import List from "@/models/list"
-import { NextResponse } from "next/server"
+import connectMongoDB from "@/libs/mongodb";
+import List from "@/models/list";
+import { NextResponse } from "next/server";
 
-export async function POST(request){
-    const { title, description} = request.json()
-    await connectMongoDB
-    await List.create({title, description})
-    return NextResponse.json({message: "List Created"}, {status: 201} )
+export async function POST(request: Request): Promise<Response> {
+  try {
+    const { title, description } = await request.json();
+    await connectMongoDB();
+
+    const newList = new List({ title, description });
+    await newList.save();
+
+    return NextResponse.json({ message: "List Created" }, { status: 201 });
+  } catch (error) {
+    console.error("Error creating list:", error);
+    return NextResponse.json({ message: "Error creating list" }, { status: 500 });
+  }
 }
