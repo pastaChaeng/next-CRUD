7110c2b8cfce3a1af8dfa95f0da8fa56ef9fbec4
Yumeko <arlenejoy.nacion777@gmail.com>
2023-09-11 10:30:45 +0800

    api

---
+++ crud/app/api/blogs/[id]/route.ts
@@ -0,0 +1,14 @@
+export const GET = async (req: Request) => {
+    console.log("GET");
+    //get a post by id
+};
+
+export const PUT = async (req: Request) => {
+    console.log("GET");
+    //update a post by id
+};
+
+export const DELETE = async (req: Request) => {
+    console.log("DELETE");
+    //delete a post by id
+};
